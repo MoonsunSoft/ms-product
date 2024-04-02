319c31d168ebd337970c4650793d1e5a7eb491dd
fardinmardani <fmardani0@gmail.com>
2024-04-02 11:08:55 +0330

    [FIX]price list in product variants just compute this

--- product_price_list/models/product.py
+++ product_price_list/models/product.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
@@ -6,7 +6,15 @@ class ProductTemplate(models.Model):
     pricelist_product_template_ids = fields.One2many(
         "product.pricelist.item",
         "product_tmpl_id",
-        string="Price List"
+        domain=lambda self: [
+            '&',
+                '|', 
+                ('product_tmpl_id', '=', self.id), 
+                ('product_id', 'in', self.product_variant_ids.ids),
+            ('pricelist_id.active', '=', True),
+        ],
+        string="Price List",
+        auto_join=True
     )
 
 
@@ -16,5 +24,21 @@ class ProductProduct(models.Model):
     pricelist_product_product_ids = fields.One2many(
         "product.pricelist.item",
         "product_id",
-        string="Price List"
+        compute="_compute_pricelist_product_ids",
+        string="Price List",
+        auto_join=True
     )
+
+    @api.depends('product_tmpl_id')
+    def _compute_pricelist_product_ids(self):
+        for product in self:
+            product.pricelist_product_product_ids = self.env['product.pricelist.item'].search([
+                ('pricelist_id.active', '=', True),
+                '|',
+                    '&',
+                    ('product_tmpl_id', '=', product.product_tmpl_id.id),
+                    ('product_id', '=', False),
+                    '&',
+                    ('product_tmpl_id', '=', product.product_tmpl_id.id),
+                    ('product_id', '=', product.id),
+            ])
