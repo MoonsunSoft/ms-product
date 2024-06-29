de7a1ec8d437f2ddd981c3a2e6aa8e35827b67d7
fardinmardani <fmardani0@gmail.com>
2024-06-29 19:07:37 +0330

    [Update]hide a field show of oca module and add manifest in show min and max quantity

---
+++ sale_restricted_qty_show_in_website/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Products Show Min and Max Quantity",
+    "version": "17.0.1.0.0",
+    "author": "ceo@moonsun.au (MoonSun)",
+    "website": "https://www.MoonSun.au",
+    "license": "AGPL-3",
+    "category": "Products",
+    "depends": ["base", "website_sale", "sale_restricted_qty"],
+    "data": [
+        "views/website.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "auto-install": False,
+}
